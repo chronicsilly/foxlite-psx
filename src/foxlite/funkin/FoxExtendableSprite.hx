a77133484a5aa74fd8fd7e518c2ee6d251e768ca
dwdvIl <60042213+dwdvIl@users.noreply.github.com>
2026-08-16 15:49:36 -0300

    Extend FunkinSprite for CodenameEngine (1.1.0)

--- src/foxlite/funkin/FoxExtendableSprite.hx
+++ src/foxlite/funkin/FoxExtendableSprite.hx
@@ -2,8 +2,9 @@ package foxlite.funkin;
 
 #if foxlite_polymod
 import funkin.modding.base.ScriptedFlxSprite;
-
 class FoxExtendableSprite extends ScriptedFlxSprite {}
+#elseif cne
+typedef FoxExtendableSprite = funkin.backend.FunkinSprite;
 #else
 typedef FoxExtendableSprite = flixel.FlxSprite;
 #end
