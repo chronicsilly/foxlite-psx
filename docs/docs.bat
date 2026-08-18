06758680debfb9b4d8ea38041251d04966b8a098
dwdvIl <60042213+dwdvIl@users.noreply.github.com>
2026-08-18 14:53:16 -0300

    add api docs gen

---
+++ docs/docs.bat
@@ -0,0 +1,3 @@
+@echo off
+haxe docs/docs.hxml
+haxelib run dox -i docs/doc.xml -o pages --title "FoxLite Documentation" -in "foxlite" --toplevel-package foxlite --keep-field-order --include-private
