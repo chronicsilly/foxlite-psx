863ededc90ef2e5812bd91db58b013eacc544c57
dwdvIl <60042213+dwdvIl@users.noreply.github.com>
2026-08-21 16:12:08 -0300

    add custom default values helper for FoxTrackData: getValueForType()

--- src/foxlite/animation/data/FoxTrackData.hx
+++ src/foxlite/animation/data/FoxTrackData.hx
@@ -1,9 +1,10 @@
 package foxlite.animation.data;
 
+import foxlite.animation.FoxTrackType;
+import foxlite.polyfill.VectorFactory;
 import lime.math.Vector2;
 import openfl.geom.Matrix3D;
 import openfl.geom.Vector3D;
-import foxlite.animation.FoxTrackType;
 
 class FoxTrackData {
 	public var frameIndex:Int = 0;
@@ -12,21 +13,31 @@ class FoxTrackData {
 	public var type #if !foxlite_polymod (default, null) #end :FoxTrackType;
 
 	public function new(_type:FoxTrackType) {
-		value = switch(_type) {
+		value = FoxTrackData.getValueForType(_type);
+		type = _type;
+	}
+
+	/**
+		Returns an object that will
+
+		@param def (Optional) Provides custom default values. For non-atomic types, must supply an array.
+	**/
+	public static function getValueForType(type:FoxTrackType, ?def:Dynamic):Any {
+		return switch(type) {
 			case FoxTrackType.ANGLE,
 				 FoxTrackType.DEGREES,
-				 FoxTrackType.FLOAT: 		0.0;
-			case FoxTrackType.BOOL: 		false;
+				 FoxTrackType.FLOAT: 		def ?? 0.0;
+			case FoxTrackType.BOOL: 		def ?? false;
 			case FoxTrackType.INT, 			
-				 FoxTrackType.COLOR:		0;
+				 FoxTrackType.COLOR:		def ?? 0;
 			case FoxTrackType.VECTOR3D, 
 				 FoxTrackType.VECTOR4, 
 				 FoxTrackType.QUATERNION, 
-				 FoxTrackType.EULER_ANGLES: new Vector3D();
-			case FoxTrackType.MATRIX4: 		new Matrix3D();
-			case FoxTrackType.VECTOR2:		new Vector2();
+				 FoxTrackType.EULER_ANGLES: def == null ? new Vector3D() : new Vector3D(def[0], def[1], def[2], def[3] ?? 0);
+			case FoxTrackType.MATRIX4: 		def == null ? new Matrix3D() : new Matrix3D(VectorFactory.Float(def));
+			case FoxTrackType.VECTOR2:		def == null ? new Vector2() : new Vector2(def[0], def[1]);
+			case FoxTrackType.FUNCTION:		def ?? ([]:Array<Dynamic>);
 			default: null;
 		}
-		type = _type;
 	}
 }
